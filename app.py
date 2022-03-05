9a0d513f649486c19abfedff169892c3da95e33e
theserioustomato <99522460+theserioustomato@users.noreply.github.com>
2022-03-05 13:25:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, request, render_template
 from textblob import TextBlob
 
 app = Flask(__name__)
 
-app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
+
 def index():
     if request.method == "POST":
         text = request.form.get("text")
         
         print(text)
         
-        out = TextBlob(d).sentiment        
+        out = TextBlob(text).sentiment        
         
         return(render_template("index.html", result=out))
         #return(render_template("index.html", result1=out1, result2=out2, result3=out3))
